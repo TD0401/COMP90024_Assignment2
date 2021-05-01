af809fd7f4e125bb56a67b0704f61fc1d4a10adf
SohamM11 <sohamm97@yahoo.in>
2021-05-01 17:44:38 +0530

    lat/long updated for avail. keys

--- ansible_scripts/run_couchDB.sh
+++ ansible_scripts/run_couchDB.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 whoami
 date
-. ./unimelb-comp90024-2021-grp-19-openrc.sh; [--ask-become-pass] couchDB-playbook.yaml > couch.txt
+. ./unimelb-comp90024-2021-grp-19-openrc.sh; ansible-playbook couchDB-playbook.yaml > couch_output.txt
+# N2VhYMYwYjIxNjFZWI0"
--- code/TwitterHarvester.py
+++ code/TwitterHarvester.py
@@ -20,43 +20,7 @@ token_list = [
         "ACCESS_TOKEN_SECRET": "Uf3dPXCJ2eJE2MyHKX4qInSJJacQkFhz1pssESk5L4xfb",
         "CONSUMER_KEY": "1TWIf5dxovtRAgsewLnAMswVf",
         "CONSUMER_SECRET": "4Fqys3SJKUaXIs929qKxS6nLJI6hLkgmid5ugn1CrlrHuAtNQ2",
-        "GEO": "37.8136,144.9631,300km",
-        "GEO_NAME": "ADELAIDE"
-    },
-    {
-        "ACCOUNT_NAME": "EG1",
-        "ACCESS_TOKEN": "1382859648613507072-8LT5wgyUWM15f1hPb4YemPY6nrONN4",
-        "ACCESS_TOKEN_SECRET": "ki18Lz1taArtHKVmrfmTzgNqkzhUlj6abJ1m5HNgSqc7T",
-        "CONSUMER_KEY": "IvftZ2VAvxMN2KTIO9VkfmArA",
-        "CONSUMER_SECRET": "q6VDoNdo4IdxuHyBy60SVkpwsu0ImQsmvbUi6reZbOcXfsuHGo",
-        "GEO": "37.8136,144.9631,300km",
-        "GEO_NAME": "SYDNEY"
-    },
-    {
-        "ACCOUNT_NAME": "HM1",
-        "ACCESS_TOKEN": "1387920378123591681-snwmk8qdEiOHxv0NUgi2xIf8g8TfyO",
-        "ACCESS_TOKEN_SECRET": "oREGZrMLSthOHZo3Ss4nBa4Vczcp2Z5QndTMsxOjwA39n",
-        "CONSUMER_KEY": "7xuNIF1O0V7qZ2wD1Vtw6FgEf",
-        "CONSUMER_SECRET": "978A7k7jjBdRsiVyfRiSBIFfKKsBwEMvEatOgSN0liljvlGULr",
-        "GEO": "37.8136,144.9631,300km",
-        "GEO_NAME": "PERTH"
-    },
-    {
-        "ACCOUNT_NAME": "HM2",
-        "ACCESS_TOKEN": "2383562348-sGkphwZZf9f6fd1mudq5rLkB6uw7jlDjKY3aFpR",
-        "ACCESS_TOKEN_SECRET": "XTLo3R8nwrAwRp1CIhHsBQ6ZIdS10RfxGlN7pCcLD2ojp",
-        "CONSUMER_KEY": "QIfwUNGd1QUmSFG1YzNjR46ST",
-        "CONSUMER_SECRET": "qyGwFSmCN3B38a9zbGh2MTfj3LiwQqaNr2wfSY4gl4ANT9vKtc",
-        "GEO": "37.8136,144.9631,300km",
-        "GEO_NAME": "BRISBANE"
-    },
-    {
-        "ACCOUNT_NAME": "AD1",
-        "ACCESS_TOKEN": "1388170110330806273-Vfxp4sE6mJNduZaA6WcgNiEkM20mfP",
-        "ACCESS_TOKEN_SECRET": "LL89KuRd89IbmWmkyL4AKc2gDEGuy9MwjnNHWCFajRc1F",
-        "CONSUMER_KEY": "IpYRKJ5BY42oQL9hmHxtTEyq5",
-        "CONSUMER_SECRET": "LS5qy6yK3AZROZfK1a6VIYHVTk3ud32DnqlxUFlJ9pfF13njKr",
-        "GEO": "37.8136,144.9631,300km",
+        "GEO": "34.9285,138.6007,300km",
         "GEO_NAME": "ADELAIDE"
     },
     {
@@ -65,8 +29,54 @@ token_list = [
         "ACCESS_TOKEN_SECRET": "CyrVLUzPDSEOlScOO6xzJfR7XYBJuBV3j1ksRDpljlJCa",
         "CONSUMER_KEY": "ZRTNsGfjmBKZk6UWKgKgLCuOp",
         "CONSUMER_SECRET": "AaFOLb2k5ZZLcV516Ht1cS8BeusN9B1GddFuMH82tc8t4YkAhP",
+        "GEO": "33.8688,151.2093,300km",
+        "GEO_NAME": "SYDNEY"
+        
+    },
+    {
+        "ACCOUNT_NAME": "HM1",
+        "ACCESS_TOKEN": "1387920378123591681-snwmk8qdEiOHxv0NUgi2xIf8g8TfyO",
+        "ACCESS_TOKEN_SECRET": "oREGZrMLSthOHZo3Ss4nBa4Vczcp2Z5QndTMsxOjwA39n",
+        "CONSUMER_KEY": "7xuNIF1O0V7qZ2wD1Vtw6FgEf",
+        "CONSUMER_SECRET": "978A7k7jjBdRsiVyfRiSBIFfKKsBwEMvEatOgSN0liljvlGULr",
+        "GEO": "31.9523,115.8613,300km",
+        "GEO_NAME": "PERTH"
+    },
+    {
+        "ACCOUNT_NAME": "HM2",
+        "ACCESS_TOKEN": "2383562348-sGkphwZZf9f6fd1mudq5rLkB6uw7jlDjKY3aFpR",
+        "ACCESS_TOKEN_SECRET": "XTLo3R8nwrAwRp1CIhHsBQ6ZIdS10RfxGlN7pCcLD2ojp",
+        "CONSUMER_KEY": "QIfwUNGd1QUmSFG1YzNjR46ST",
+        "CONSUMER_SECRET": "qyGwFSmCN3B38a9zbGh2MTfj3LiwQqaNr2wfSY4gl4ANT9vKtc",
+        "GEO": "27.4705,153.0260,300km",
+        "GEO_NAME": "BRISBANE"
+    },
+    {
+        "ACCOUNT_NAME": "AD1",
+        "ACCESS_TOKEN": "1388170110330806273-Vfxp4sE6mJNduZaA6WcgNiEkM20mfP",
+        "ACCESS_TOKEN_SECRET": "LL89KuRd89IbmWmkyL4AKc2gDEGuy9MwjnNHWCFajRc1F",
+        "CONSUMER_KEY": "IpYRKJ5BY42oQL9hmHxtTEyq5",
+        "CONSUMER_SECRET": "LS5qy6yK3AZROZfK1a6VIYHVTk3ud32DnqlxUFlJ9pfF13njKr",
+        "GEO": "35.2809,149.1300,300km",
+        "GEO_NAME": "CANBERRA"
+    },
+    {
+        "ACCOUNT_NAME": "EG1",
+        "ACCESS_TOKEN": "1382859648613507072-8LT5wgyUWM15f1hPb4YemPY6nrONN4",
+        "ACCESS_TOKEN_SECRET": "ki18Lz1taArtHKVmrfmTzgNqkzhUlj6abJ1m5HNgSqc7T",
+        "CONSUMER_KEY": "IvftZ2VAvxMN2KTIO9VkfmArA",
+        "CONSUMER_SECRET": "q6VDoNdo4IdxuHyBy60SVkpwsu0ImQsmvbUi6reZbOcXfsuHGo",
+        "GEO": "12.4637,130.8444,300km",
+        "GEO_NAME": "DARWIN"
+    },
+    {
+        "ACCOUNT_NAME": "EG2",
+        "ACCESS_TOKEN": "1388356373797171206-xro1a2wXQLQ9XRppkUaJoRHeNNPUIf",
+        "ACCESS_TOKEN_SECRET": "jplA7qTRP5Ou4bombZVylanK5nLefFEl4sKp5lb2BL1ij",
+        "CONSUMER_KEY": "sRqz4V2OdLOfpifO813ekzcOd",
+        "CONSUMER_SECRET": "Dv6xpUVwCWEYH5mko04AH7mgpejBLjfhHyEYXl8b2NFhXlMjVg",
         "GEO": "37.8136,144.9631,300km",
-        "GEO_NAME": "ADELAIDE"
+        "GEO_NAME": "MELBOURNE"
     }
 ]
 COUCH_URL = "http://admin:password@127.0.0.1:5984/"
@@ -87,7 +97,9 @@ def parse_and_store(db, tweet_json, index):
         id = tweet_json['id']
         json_data = {'_id': "partition" + str(index) + ":" + str(id),
                     'created_at':tweet_json['created_at'],
-                     'tweet_text':tweet_json['text']
+                     'text':tweet_json['text']
+                     'hashtags':tweet_json['hastag']
+                     "[user:id]":tweet_json['usr_id']
                      ## TODO: need to complete the json part
         }
         db.save(json_data)
