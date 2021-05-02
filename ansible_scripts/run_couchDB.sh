924092d9c4a80400f025dc14f4c0f68228b65953
SohamM11 <sohamm97@yahoo.in>
2021-05-02 12:32:25 +0530

    DO NOT RUN ./run_couchDB or couchDB_playbook again. Updated output.txt

--- ansible_scripts/run_couchDB.sh
+++ ansible_scripts/run_couchDB.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+#this sh allows the couchDB-playbook.yaml to launch two instances on the MRC
+#of 120 GB volume each; as configured in the roles
+
 whoami
 date
-. ./unimelb-comp90024-2021-grp-19-openrc.sh; ansible-playbook couchDB-playbook.yaml > couch_output.txt
-# N2VhYMYwYjIxNjFZWI0
+. ./unimelb-comp90024-2021-grp-19-openrc.sh; ansible-playbook --ask-become-pass couchDB-playbook.yaml > couch_output.txt
+# N2VhYmYwYjIxNjFkZWI0
