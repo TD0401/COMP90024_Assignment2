e60fab937dd5ffc26f30cc785508b3bf2fe97186
SohamM11 <sohamm97@yahoo.in>
2021-05-04 04:04:21 +1000

    made some changes - should work!

---
+++ ansible_scripts/run_harvester.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#this sh allows the harvester-playbook.yaml to launch one instance on the MRC
+#vol: 50 GB volume each; installs docker and tweepy for running the harvester py file.
+
+whoami
+date
+. ./unimelb-comp90024-2021-grp-19-openrc.sh; ansible-playbook --ask-become-pass harvester-playbook.yaml > harvester_out.txt
+# N2VhYmYwYjIxNjFkZWI0
